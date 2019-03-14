495b17862cbcd3861e06ece3f86d6d22e45dfffb
Deric Walintukan <deric.walintukan@gmail.com>
2019-03-14 12:29:12 +0700

    Replace util methods with fixed versions

--- contracts/lib/Utils.sol
+++ contracts/lib/Utils.sol
@@ -2,17 +2,23 @@ pragma solidity ^0.5.4;
 
 library Utils {
     function validateNotHex(bytes memory b) internal pure returns (bool) {
-        return b[0] != 0x30 && b[1] != 0x78;
+        // Check length before trying to access at index
+        if (b.length >= 2) {
+            return b[0] != 0x30 && b[1] != 0x78;
+        }
+        return true;
     }
 
     function validateLettersAndNumbers(bytes memory b) internal pure returns (bool) {
         for (uint i = 0; i < b.length; i++) {
+            bytes1 char = b[i];
+
             // See ASCII table for reference: http://www.asciitable.com/
+            // If not 0-9,A-Z, or a-z, return not valid.
             if (
-                b[i] < 0x30 ||                  // b < 0
-                (b[i] > 0x39 && b[i] < 0x41) || // b > 9 && b < A
-                (b[i] > 0x5a && b[i] < 0x61) || // b > Z && b < a
-                b[i] > 0x7a                     // b > z
+                !(char >= 0x30 && char <= 0x39) && // 0-9
+                !(char >= 0x41 && char <= 0x5A) && // A-Z
+                !(char >= 0x61 && char <= 0x7A)    // a-z
             ) {
                 return false;
             }
